42040d64584db1a3b8a0a82caed5a50e15b6086d
winnning godspower <winnninggodspower@gmail.com>
2025-01-31 02:09:57 +0100

    feat: add django defaults static file finders to settings

--- medics_advantage/settings.py
+++ medics_advantage/settings.py
@@ -81,7 +81,11 @@ COMPRESS_ROOT = BASE_DIR / 'static'
 
 COMPRESS_ENABLED = True
 
-STATICFILES_FINDERS = ('compressor.finders.CompressorFinder',)
+STATICFILES_FINDERS = (
+    'compressor.finders.CompressorFinder',
+    "django.contrib.staticfiles.finders.FileSystemFinder",
+    "django.contrib.staticfiles.finders.AppDirectoriesFinder",
+    )
 
 
 # Database

9016a8808a79323b5eac9d5d085be7a130efff5a
winnning godspower <winnninggodspower@gmail.com>
2025-02-10 15:08:40 +0100

    fix: update import statement for Booking model to use core namespace

--- core/views.py
+++ core/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from django.core.mail import send_mail
 from django.conf import settings
-from .models import Booking
+from core.models import Booking
 import paypalrestsdk
 import datetime
 

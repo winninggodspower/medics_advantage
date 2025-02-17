c8a4d7f3c38202830aa2a37fd6654e0fcdb083dd
winnning godspower <winnninggodspower@gmail.com>
2025-02-17 23:52:53 +0100

    feat: optimize site images with django_img_optimizer package

--- medics_advantage/settings.py
+++ medics_advantage/settings.py
@@ -41,7 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     
     "django_browser_reload",
-    # 'compressor',
+    'django_img_optimizer',
     "core",
 ]
 
@@ -175,6 +175,7 @@ STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
 STATIC_ROOT = os.path.join(BASE_DIR, "../public_html/static")
+OPTIMIZE_IMAGE_ROOT = os.path.join(BASE_DIR, 'static', 'images')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field

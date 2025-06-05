da95e6dcbdf58efefa609c98560b6597c1cdafc7
winning godspower <winninggodspower@gmail.com>
2025-06-05 20:42:31 +0100

    feat: update video field to use URLField for YouTube links and enhance video modal functionality

--- video_lectures/models.py
+++ video_lectures/models.py
@@ -3,7 +3,7 @@ from django.db import models
 class VideoLecture(models.Model):
     title = models.CharField(max_length=255)
     description = models.TextField()
-    video = models.FileField(upload_to='video_lectures/')
+    video = models.URLField(max_length=255, help_text="YouTube video URL")
     thumbnail = models.ImageField(upload_to='video_lectures/thumbnails/', null=True, blank=True)
 
     def __str__(self):
